1253fcba07ddcb82e1f063bd5010628dfa0c42ea
Robertus Sonny Prakoso <robertussonnyprakoso@live.com>
2017-01-30 15:43:11 +0700

    edit javascript function

--- blockgenerator.js
+++ blockgenerator.js
@@ -1,17 +1,22 @@
 
 (function ($) { 
   // Code that uses jQuery's $ can follow here..
-  $.ajax({
+  
+  Drupal.behaviors.BlockVisibility = {
+    attach: function (context, settings) {
+        $.ajax({
         url: Drupal.settings.basePath + 'blockgenerator/get/country',
         method: 'POST',
         dataType: 'json',
         success: function (data) {
             if (data.country_code) {
-                $('.allow_all:not(.hide_' + data.country_code + ')').removeClass('element-invisible');
-                $('.hide_all.allow_' + data.country_code).removeClass('element-invisible');
+                $('.allow_all:not(.hide_' + data.country_code + ')').show();
+                $('.hide_all.allow_' + data.country_code).show();
             } else {
-                $('.allow_all').removeClass('element-invisible');
+                $('.allow_all').show();
             }
         }
     });
+    }
+  }
 })(jQuery);
