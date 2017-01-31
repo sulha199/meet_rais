31b9c3fe419d2f0cd2e2c080ed80e7eb91d4c45c
Robertus Sonny Prakoso <robertussonnyprakoso@live.com>
2017-01-31 13:49:34 +0700

    fix error for owl carousel block

--- blockgenerator.js
+++ blockgenerator.js
@@ -1,22 +1,27 @@
 
 (function ($) { 
   // Code that uses jQuery's $ can follow here..
-  
-  Drupal.behaviors.BlockVisibility = {
-    attach: function (context, settings) {
-        $.ajax({
+    $.ajax({
         url: Drupal.settings.basePath + 'blockgenerator/get/country',
         method: 'POST',
         dataType: 'json',
         success: function (data) {
             if (data.country_code) {
-                $('.allow_all:not(.hide_' + data.country_code + ')').show();
-                $('.hide_all.allow_' + data.country_code).show();
+                $('.allow_all:not(.hide_' + data.country_code + ')').removeClass('element-hidden');
+                $('.hide_all.allow_' + data.country_code).removeClass('element-hidden');
             } else {
-                $('.allow_all').show();
+                $('.allow_all').removeClass('element-hidden');
             }
+
+            //fix owlcarousel after re-showing element
+            $('.owl-carousel').each(function() {
+                if($(this).closest('.element-hidden').length == 0) {
+                    $(this).trigger('destroy.owl.carousel');
+                    $(this).html($(this).find('.owl-stage-outer').html()).removeClass('owl-loaded');
+                    $(this).owlCarousel(Drupal.settings.owlcarousel[this.id].settings);
+                }
+            })
+            
         }
     });
-    }
-  }
 })(jQuery);
